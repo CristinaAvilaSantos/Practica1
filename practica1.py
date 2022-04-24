dcb2e4da4f4aed45d5f3c869c0e1b1eebb2b3d96
CristinaAvilaSantos <98969563+CristinaAvilaSantos@users.noreply.github.com>
2022-04-24 19:40:36 +0200

    Correccion de la practica1

--- practica1.py
+++ practica1.py
@@ -1,122 +1,102 @@
-from multiprocessing import Process, BoundedSemaphore, Semaphore, Lock, current_process, Value, Array
+from multiprocessing import Process, Semaphore, Lock, Value, Array
 from time import sleep
 from random import randint, random
 
-N = 100
-K = 10
-NPROD = 3
-
+N = 4
+NPROD = 2
+K = NPROD * 4
 
 
 def delay(factor = 3):
     sleep(random()/factor)
-    
-def ord_insercion(lista, dato):
-    todo_ceros = True
-    for i in range(len(lista)):
-        if lista[i] != 0: 
-            todo_ceros = False
-        else:
-            lista[0] = dato
-            break
-        if dato <= lista[i] and todo_ceros == False:  
-            j = i+1
-            while j > 0 and dato < lista[j-1]:
-                for k in range(len(lista)-1, i, -1):
-                    lista[k] = lista[k-1]
-                lista[j] = lista[j-1]
-                j -= 1
-                lista[j] = dato  
-            break
-        elif dato > lista[i] and lista[i+1] == 0:
-            lista[i+1] = dato
-            break
-    print(lista) 
-    return lista
-   
-   
-def produce_values(producidos, data, mutex):
+
+def anadir(resultados, v, mutex, index):
     mutex.acquire()
     try:
-        delay(5)
-        i = 1
-        x = 0
-        while i > 0 and i < K:
-            if producidos[i] == 0:
-                producidos[i] = data + randint(1, 20)
-                x = producidos[i]
-                break
-            i += 1
+        resultados[index.value] = v
+        index.value += 1
     finally:
         mutex.release()
-    return x
-        
+                        
+
+def add(producidos, v, mutex, pid):
+    mutex.acquire()
+    try:
+        producidos[pid] = v
+    finally:
+        mutex.release()
+                    
+                    
 def get_minvalues(producidos, mutex):
     mutex.acquire()
     try:
         menor = 10**6
         pmenor = -1
-        for i in range(K):
-            if producidos[i] > 0:
-                if producidos[i] < menor:
-                    pmenor = i
-                    menor = producidos[i]
-        if pmenor != -1 :
-           menor = producidos[pmenor]
-           producidos[pmenor] = 0
-        else: 
-           menor = -1
-           print(f"{current_process().name} = -1")
+        i = 1
+        for i in range(NPROD):
+            if (producidos[i] !=1) and (producidos[i] < menor):
+                pmenor = i
+                menor = producidos[i]
     finally:
         mutex.release()
-    return menor
+    return pmenor
             
             
-def producer(producidos, empty, non_empty, mutex):
+def producer(l, producidos, empty, nonEmpty, mutex, pid):
     x = 0
-    for v in range(N):
-        print (f"{current_process().name} produciendo")
+    for i in range(l):
+        print (f"productor {pid} produciendo")
         delay(6)
         empty.acquire()
-        x = produce_values(producidos, x, mutex)
-        print(list(producidos))       
-        print(f"Productor {current_process().name} ha producido", x,v)
+        x += randint(0,20)
+        add(producidos,x, mutex, pid)
+        print(f"Productor {pid} ha producido", x)
         delay(10)
-        non_empty.release()  
+        nonEmpty.release()  
+    empty.acquire()
+    producidos[pid] = -1
+    nonEmpty.release()
  
-def consumer(producidos, resultados, empty, non_empty, mutex):
+def consumer(producidos, resultados, empty, non_empty, mutex, index):
     print ('entra en consumer')
-    for v in range(N):
-        non_empty.acquire()
-        print ('Desalmacenando el menor')
-        dato = get_minvalues(producidos, mutex)
-        empty.release()
-        print (f"colocando {dato}")
-        resultados = ord_insercion(list(resultados), dato)
-        print(f"lista final=", list(resultados))
+    for i in range(NPROD):
+        non_empty[i].acquire()
+    while (-1) in resultados:
+        minP = get_minvalues(producidos, mutex)
+        anadir(resultados, producidos[minP], mutex, index)
+        print(resultados[:])
+        empty[minP].release()
+        print (f"consumer  consumiendo {producidos[minP]}")
+        non_empty[minP].acquire()
 
 
 def main():
-    resultados = Array('i', N)
-    producidos = Array('i', K)
+    resultados = Array('i', K)
+    producidos = Array('i', NPROD)
     index = Value('i', 0)
-    menor = producidos[0]
+    
+    for i in range(NPROD):
+        producidos[i] = -1
    
-    for i in range(K-1):
-        producidos[i]=producidos[i+1]
+    for i in range(K):
+        resultados[i] = -1
         
-    non_empty = Semaphore(0)
-    empty = BoundedSemaphore(K)
+    non_empty = []			
+    empty = []				
     mutex = Lock()
+    
+    for i in range(NPROD):
+        empty.append(Semaphore())
+        non_empty.append(Semaphore())
 
     prodlst = [ Process(target=producer,
                         name=f'prod_{i}',
-                        args=(producidos, empty, non_empty, mutex))
+                        args=(N, producidos, empty[i], non_empty[i], mutex, i))
                 for i in range(NPROD) ]
 
     conslst = [Process(target=consumer,
                       name=f"cons_{i}",
-                      args=(producidos, resultados, empty, non_empty, mutex)) ]
+                      args=(producidos, resultados, empty, non_empty, mutex, index)) ]
           
     for p in prodlst + conslst:
         p.start()
